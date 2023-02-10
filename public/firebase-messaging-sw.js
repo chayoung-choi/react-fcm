cb77228195b98cbc0b0c0c3221768e154aa4f15b
chayoung-choi <chayoung.dev@gmail.com>
2023-02-10 18:02:48 +0900

    feat: firebase push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function (payload) {
   console.log('Received background message ', payload);
 
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = "[ServiceBack] " + payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
     icon: "/favicon.ico"
